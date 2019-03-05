f2a23e03f2f7074e25c3c6d0f03354206b674909
Kurt Miller <bsdkurt@gmail.com>
2019-03-05 14:50:16 -0500

    Add src support for *BSD that are sufficient to build a native bootjdk: - Port jdk8 bsd-port changes to jdk11 and resolve any new compilation issues. Notable items in this commit: - Use new _BSDONLY_SOURCE for places where _ALLBSD_SOURCE was not appropriate - java.base/unix/native/libnio/ch/FileDispatcherImpl.c: Use #defines for   statvfs64 and fstatvfs64 for _ALLBSD_SOURCE instead of #ifdef MACOSX - Start with a copy of linux_close.c (poll based) for bsd_close.c in   java.base/bsd/native/libnet - Start with a copy of ProcessHandleImpl_macosx.c for ProcessHandleImpl_bsd.c   in java.base/bsd/native/libjava. Some items left to do: - hotspot/os/bsd/os_perf_bsd.cpp: network_utilization BSD implementation. - java.base/bsd/native/libjava/ProcessHandleImpl_bsd.c: os_getChildren,   os_getParentPidAndTimings and os_getCmdlineAndUserInfo implementations. - Removal of direct use of _BIG_ENDIAN and _LITTLE_ENDIAN in source which   doesn't work on *BSD and use VM_BIG_ENDIAN VM_LITTLE_ENDIAN instead. - BSD PPC64 support - Careful review of jdk8 bsd-port patches to ensure all changes made it   into jdk11

--- src/src/java.base/unix/native/libjava/TimeZone_md.c
+++ src/src/java.base/unix/native/libjava/TimeZone_md.c
@@ -62,7 +62,11 @@
 #endif
 
 #if defined(__linux__) || defined(_ALLBSD_SOURCE)
+#ifdef __FreeBSD__
+static const char *ETC_TIMEZONE_FILE = "/var/db/zoneinfo";
+#else
 static const char *ETC_TIMEZONE_FILE = "/etc/timezone";
+#endif
 static const char *ZONEINFO_DIR = "/usr/share/zoneinfo";
 static const char *DEFAULT_ZONEINFO_FILE = "/etc/localtime";
 #else
@@ -75,7 +79,7 @@ static const char *DEFAULT_ZONEINFO_FILE = "/usr/share/lib/zoneinfo/localtime";
 static const char *ETC_ENVIRONMENT_FILE = "/etc/environment";
 #endif
 
-#if defined(__linux__) || defined(MACOSX) || defined(__solaris__)
+#if defined(__linux__) || defined(_ALLBSD_SOURCE) || defined(__solaris__)
 
 /*
  * Returns a pointer to the zone ID portion of the given zoneinfo file
@@ -215,7 +219,7 @@ findZoneinfoFile(char *buf, size_t size, const char *dir)
     return tz;
 }
 
-#if defined(__linux__) || defined(MACOSX)
+#if defined(__linux__) || defined(_ALLBSD_SOURCE)
 
 /*
  * Performs Linux specific mapping and returns a zone ID
@@ -232,7 +236,7 @@ getPlatformTimeZoneID()
     size_t size;
     int res;
 
-#if defined(__linux__)
+#if defined(__linux__) || defined(_BSDONLY_SOURCE) 
     /*
      * Try reading the /etc/timezone file for Debian distros. There's
      * no spec of the file format available. This parsing assumes that
@@ -256,7 +260,7 @@ getPlatformTimeZoneID()
             return tz;
         }
     }
-#endif /* defined(__linux__) */
+#endif /* defined(__linux__) || defined(_BSDONLY_SOURCE) */
 
     /*
      * Next, try /etc/localtime to find the zone ID.
@@ -827,7 +831,7 @@ findJavaTZ_md(const char *java_home_dir)
  * Returns a GMT-offset-based zone ID. (e.g., "GMT-08:00")
  */
 
-#if defined(MACOSX)
+#if defined(_ALLBSD_SOURCE)
 
 char *
 getGMTOffsetID()
@@ -892,4 +896,4 @@ getGMTOffsetID()
             sign, (int)(offset/3600), (int)((offset%3600)/60));
     return strdup(buf);
 }
-#endif /* MACOSX */
+#endif /* _ALLBSD_SOURCE */
