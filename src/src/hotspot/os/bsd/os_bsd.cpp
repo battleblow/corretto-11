01d9585e6f2dd3a25fd98512fe5e0ca9fdbf2fdd
Greg Lewis <glewis@eyesbeyond.com>
2019-03-30 08:58:04 -0700

    Add missing return statement
    * Fix the FreeBSD build by adding a missing return statement in
      os::set_native_priority for the FreeBSD preprocessor branch.

--- src/src/hotspot/os/bsd/os_bsd.cpp
+++ src/src/hotspot/os/bsd/os_bsd.cpp
@@ -2634,6 +2634,7 @@ OSReturn os::set_native_priority(Thread* thread, int newpri) {
   return OS_OK;
 #elif defined(__FreeBSD__)
   int ret = pthread_setprio(thread->osthread()->pthread_id(), newpri);
+  return (ret == 0) ? OS_OK : OS_ERR;
 #elif defined(__APPLE__) || defined(__NetBSD__)
   struct sched_param sp;
   int policy;
